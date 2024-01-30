a0fb5d701a26873c033e1cd6bad613407e93db90
Emeline <emeline.courcelle@gmail.com>
2024-01-30 12:45:52 +0100

    new routes

--- routes/bookings.js
+++ routes/bookings.js
@@ -20,20 +20,26 @@ newBooking.save().then(() => res.json({ result: true }))
 });
 
 /* UPDATE booking. */
-router.post('/new', function (req, res) {
-    const { departure, arrival, date, price } = req.body
-  
-    const newBooking = new Booking({
-      departure,
-      arrival,
-      date,
-      price,
-      isPaid: false
-  })
-  
-  newBooking.save().then(() => res.json({ result: true }))
-  
-  });
+router.put("/update", (req, res) => {
+    const { _id } = req.body
+
+    Booking.findOne({ _id }).then((tripFound) => {
+        if (!tripFound) {
+            return res.json({ result: false, error: "Trip not found" })
+        } else {
+            Booking.updateOne({ _id }, { isPaid: true }).then((tripUpdated) => {
+                return res.json({ result: true, tripUpdated })
+            })
+        }
+    })
+})
+
+/* DELETE booking */
+router.delete("/delete", (req, res) => {
+    Booking.deleteMany({ isPaid: false }).then((tripsDeleted) => {
+        return res.json({ result: true, tripsDeleted })
+       });
+})
 
 module.exports = router;
 

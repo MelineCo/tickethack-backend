ebd47acb45242321877f1c00b4de17d9fe1a77a3
Antoine <antoine.beppo@gmail.com>
2024-01-30 11:21:59 +0100

    models et schema trips booking connection

--- models/connection.js
+++ models/connection.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose');
-
-const connectionString = 'mongodb+srv://Abeps:VO1I3evrIlExvqg3@abeps.xxkoe0e.mongodb.net/tickethack';
+/// VARIABLE D'ENVIRONNEMENT.
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
 .then(() => console.log('Database connected'))
 .catch(error => console.error(error));
 
 
-  // A rajouter dans fichier  :
-
-
-//   require('./models/connection');
